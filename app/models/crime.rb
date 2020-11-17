50561a5c1324ffd4832d819d161749d8f8f5af20
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-17 11:07:42 +0000

    Crime and Skillset validations, User association with Skillset and Crime

--- app/models/crime.rb
+++ app/models/crime.rb
@@ -1,5 +1,13 @@
 class Crime < ApplicationRecord
   has_one :skillset
   belongs_to :boss, class_name: 'User'
-  belongs_to :right_hand, class_name: 'User'
+  belongs_to :right_hand, class_name: 'User', optional: true
+
+  validates_associated :skillset
+
+  validates :title, length: { minimum: 5 }, allow_blank: false
+  # For testing purposes the description length validation is shorter. Must change this before production
+  validates :description, length: { minimum: 5 }, allow_blank: false
+  validates :date, :location, presence: true
+  validates :reward, numericality: true
 end
