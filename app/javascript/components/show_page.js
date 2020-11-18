f553d80ed0ba09710c2d363672eebea3c1b4485d
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-18 15:45:26 +0000

    Create event page position

--- app/javascript/components/show_page.js
+++ app/javascript/components/show_page.js
@@ -1,6 +1,7 @@
 const toggleNewEventPage = () => {
   $('#create-event-btn').click(function(e) {
     e.preventDefault();
+    $('#profile-dashboard-container').toggle();
     $('#create-event-page').toggle();
   });
 }
