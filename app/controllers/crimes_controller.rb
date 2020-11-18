ac0bb7de177baec8771ba6a0c133c06bed85bdde
Mathias Leniau <mathiasleniau@gmail.com>
2020-11-18 15:10:47 +0000

    Crimes Controller Done

---
+++ app/controllers/crimes_controller.rb
@@ -0,0 +1,66 @@
+class CrimesController < ApplicationController
+  before_action :set_crime, except: [:index, :new, :create]
+  before_action :permitted_skillset_attributes, only: [:show]
+
+  def index
+    @crimes = Crime.all
+  end
+
+  def show
+  end
+
+  def new
+    @crime = Crime.new
+  end
+
+  def create
+    @crime = Crime.new(crime_params)
+    @crime.right_hand = User.find(right_hand_param[:right_hand]) unless right_hand_param[:right_hand].empty?
+    
+    @crime.boss = current_user
+
+    if @crime.save
+      # We may need to change this redirect path.
+      redirect_to profile_path, notice: 'Your plan was added to the database!'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @crime.right_hand = User.find(right_hand_param[:right_hand]) unless right_hand_param[:right_hand].empty?
+
+    if @crime.update(crime_params)
+      redirect_to crime_path(@crime), notice: 'Plan updated!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @crime.destroy
+    # We may need to change this redirect path.
+    redirect_to profile_path
+  end
+
+  private
+
+  def set_crime
+    @crime = Crime.find(params[:id])
+  end
+
+  def permitted_skillset_attributes
+    @attributes = ['id', 'created_at', 'updated_at', 'user_id', 'crime_id']
+  end
+
+  def crime_params
+    params.require(:crime).permit(:title, :description, :date, :location, :reward)
+  end
+
+  def right_hand_param
+    params.require(:crime).permit(:right_hand)
+  end
+end
