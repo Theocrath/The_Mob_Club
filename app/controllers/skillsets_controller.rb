b0f15de6f116a21696902203371b5ecc4d2fdbf1
Oriana Mata <oribouros@gmail.com>
2020-11-19 15:07:26 +0000

    added two new routes to create a skillset for the crime

--- app/controllers/skillsets_controller.rb
+++ app/controllers/skillsets_controller.rb
@@ -6,7 +6,7 @@ class SkillsetsController < ApplicationController
 
   def create
     @user = current_user
-    @skillset = Skillset.new(skillset_params)
+    @skillset = Skillset.new(skillset_params_user)
     @skillset.user = @user
 
     if @skillset.save
@@ -16,9 +16,29 @@ class SkillsetsController < ApplicationController
     end
   end
 
+  def another_new
+    @skillset = Skillset.new
+  end
+
+  def another_create
+    @user = current_user
+    @skillset = Skillset.new(skillset_params_crime)
+    @skillset.crime = @crime
+
+    if @skillset.save
+      redirect_to profile_path, notice: 'Skillset created for your crime!'
+    else
+      render :new
+    end
+  end
+
   private
 
-  def skillset_params
+  def skillset_params_user
     params.require(:skillset).permit(:close_combat, :long_range_weapons, :mid_range_weapons, :explosives, :infiltration, :hacking, :lockpicking, :seduction, :user_id)
+  end
+
+  def skillset_params_crime
+    params.require(:skillset).permit(:close_combat, :long_range_weapons, :mid_range_weapons, :explosives, :infiltration, :hacking, :lockpicking, :seduction, :crime_id)
   end
 end
