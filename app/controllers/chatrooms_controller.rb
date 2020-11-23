63fdf232f7553007ae249a7ba76a36731e1540a4
Oriana Mata <oribouros@gmail.com>
2020-11-23 18:27:53 +0000

    index, new and create actions implemented

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,33 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatroom = Chatroom.all
+  end
+
+  def new
+    @crime = Crime.find(params[:crime_id])
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @crime = Crime.find(params[:crime_id])
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.name = @crime.title
+    # @crime = Crime.find(params[:crime_id])
+    if @chatroom.save
+      redirect_to crime_chatrooms_path, notice: 'Chatroom created.'
+    else
+      render "chatrooms/show"
+    end
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:name)
   end
 end
