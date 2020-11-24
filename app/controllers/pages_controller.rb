56c1fb3ddad72f1598a8e70cd6b47565f1dadcfe
Joana Bertoldi <joanasoares_1994@msn.com>
2020-11-24 11:41:18 +0000

    Rankings updated to have next level and points displayed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   before_action :set_current_jobs, only: :profile
   before_action :set_pending_requests, only: :profile
   before_action :set_ranking, only: :profile
+  before_action :set_next_ranking, only: :profile
 
   def home
   end
@@ -64,6 +65,27 @@ class PagesController < ApplicationController
       when 60..74 then @ranking = 'Expert Havoc Raiser'
       when 75..89 then @ranking = 'Master Criminal'
       when 90..100 then @ranking = 'Crime Overlord'
+    end
+  end
+
+  def set_next_ranking
+    case stats_average
+      when 0..19 
+        @next_ranking = 'Common Wrongdoer'
+        @until_next_ranking = 20 - stats_average
+      when 20..39
+        @next_ranking = 'Standard Law Defiant'
+        @until_next_ranking = 40 - stats_average
+      when 40..59
+        @next_ranking = 'Expert Havoc Raiser'
+        @until_next_ranking = 60 - stats_average
+      when 60..74
+        @next_ranking = 'Master Criminal'
+        @until_next_ranking = 75 - stats_average
+      when 75..89
+        @next_ranking = 'Crime Overlord'
+        @until_next_ranking = 90 - stats_average
+      when 90..100 then @next_ranking = 'You reached The Godfather level'
     end
   end
 end
